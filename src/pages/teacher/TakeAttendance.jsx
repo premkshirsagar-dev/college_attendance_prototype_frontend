a5c7904183bfb5af31c95b2dd14bd7dca238baca
Prem_kshirsagar <premkshirsagar.dev@gmail.com>
2026-09-15 10:29:08 +0530

    Update import path for CLASS_OPTIONS

--- src/pages/teacher/TakeAttendance.jsx
+++ src/pages/teacher/TakeAttendance.jsx
@@ -7,7 +7,7 @@ import api from "../../api/axios";
 import Navbar from "../../components/Navbar";
 import Alert from "../../components/Alert";
 
-import { CLASS_OPTIONS } from "../../constants/classes";
+import { CLASS_OPTIONS } from "../../context/classes";
 const todayISO = () => new Date().toISOString().slice(0, 10);
 
 const TakeAttendance = () => {
