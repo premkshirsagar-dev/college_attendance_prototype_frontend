c952c268c6c2be5ce178f2378b4477ce224a045e
Prem_kshirsagar <premkshirsagar.dev@gmail.com>
2026-09-15 10:27:55 +0530

    Update import path for CLASS_OPTIONS

--- src/pages/teacher/AttendanceRecords.jsx
+++ src/pages/teacher/AttendanceRecords.jsx
@@ -8,7 +8,7 @@ import api from "../../api/axios";
 import Navbar from "../../components/Navbar";
 import Alert from "../../components/Alert";
 import Alert from "../../components/Alert";
-import { CLASS_OPTIONS as ALL_CLASSES } from "../../constants/classes";
+import { CLASS_OPTIONS as ALL_CLASSES } from "../../context/classes";
 
 const CLASS_OPTIONS = ["", ...ALL_CLASSES];
 const STATUS_OPTIONS = ["", "Present", "Absent"];
