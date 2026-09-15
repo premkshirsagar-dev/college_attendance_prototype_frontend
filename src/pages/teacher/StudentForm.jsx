682476774b160765e4fb5e8f16c5a57e6400aa0d
Prem_kshirsagar <premkshirsagar.dev@gmail.com>
2026-09-15 10:28:48 +0530

    Update import path for CLASS_OPTIONS

--- src/pages/teacher/StudentForm.jsx
+++ src/pages/teacher/StudentForm.jsx
@@ -7,7 +7,7 @@ import api from "../../api/axios";
 import Navbar from "../../components/Navbar";
 import Alert from "../../components/Alert";
 
-import { CLASS_OPTIONS } from "../../constants/classes";
+import { CLASS_OPTIONS } from "../../context/classes";
 const StudentForm = () => {
   const { id } = useParams();
   const isEdit = Boolean(id);
