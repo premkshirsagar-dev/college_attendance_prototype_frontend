61c53ff1b8190a04a52d54a371cd896c950e3382
Prem_kshirsagar <premkshirsagar.dev@gmail.com>
2026-09-18 16:31:20 +0530

    Update import path for ManageStudents component

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import StudentDashboard from "./pages/student/StudentDashboard";
 import StudentProfile from "./pages/student/StudentProfile";
 
 import TeacherDashboard from "./pages/teacher/TeacherDashboard";
-import ManageStudents from "./pages/ManageStudents";
+import ManageStudents from "./pages/student/ManageStudents";
 import TakeAttendance from "./pages/teacher/TakeAttendance";
 import AttendanceRecords from "./pages/teacher/AttendanceRecords";
 import MidwayLeavers from "./pages/teacher/MidwayLeavers";
